6104a0d20f7782a8ba0b981df99de9fdd20e1b69
Santiago Mendoza Ramirez <contacto@santiagomendoza.org>
2015-02-10 10:00:19 -0500

    Menu collapsed en moviles
    Menú collapsed en moviles.

--- resources/views/templates/master.blade.php
+++ resources/views/templates/master.blade.php
@@ -9,9 +9,14 @@
     <body>
         <div id="container" class="container-fluid">
             @section('header')
+            <div id="sidebar-button" class="visible-xs-block">
+                <button id="buttontoggle" type="button" class="btn btn-primary" onclick="toggleSidebar()">
+                    <span class="glyphicon glyphicon-align-justify" aria-hidden="true"></span>
+                </button>
+            </div>
             <div data-spy="affix" id="sidebar-container" class="col-sm-2 hidden-xs">
                 <div id="sidebar" class="col-sm-12">
-                    <h2><a href="/">Accounting Book</a></h2>
+                    <h2 id="site-tittle"><a href="/">Accounting Book</a></h2>
                     <div id="profileinfo" class="media">
                         <div class="media-left">
                             <a href="/user">
@@ -34,15 +39,6 @@
                     </nav>
                 </div>
             </div>
-            <nav class="navbar visible-xs-inline">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#sidebar-container">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a href="/" class="brand">AccountingBook</a>
-            </nav>
             @show
             <div id="content-container" class="col-sm-10 col-sm-offset-2">
                 @yield('content')
@@ -55,5 +51,14 @@
         </div>
         <script src="/js/jquery.min.js"></script>
         <script src="/js/bootstrap.min.js"></script>
+        <script>
+                    function toggleSidebar() {
+                        if ($('#sidebar-container').hasClass('hidden-xs')) {
+                            $('#sidebar-container').removeClass('hidden-xs');
+                        } else {
+                            $('#sidebar-container').addClass('hidden-xs');
+                        }
+                    }
+        </script>
     </body>
 </html>
