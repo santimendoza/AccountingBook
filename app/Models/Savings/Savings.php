21fe08a0fb88b6f407ceed00cb36bd2ce71ba34b
Santiago Mendoza Ramirez <contacto@santiagomendoza.org>
2015-02-25 08:57:30 -0500

    SoftDelete a Ahorros
    Agregado softDeletes a ahorros, para que al borrar el ahorro no se
    borren las "agregadas de saldo", para mantener un informe o reporte de
    todos los movimientos.
    
    Además de poder revisar historicamente los ahorros.

--- app/Models/Savings/Savings.php
+++ app/Models/Savings/Savings.php
@@ -3,11 +3,15 @@
 namespace App\Models\Savings;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Savings extends Model {
 
+    use SoftDeletes;
+
     protected $table = 'savings';
     protected $fillable = array('amount', 'description', 'title', 'addedfounds', 'user_id');
+    protected $dates = array('deleted_at');
 
     public function users() {
         return $this->belongsTo('User', 'user_id');
--- database/migrations/2015_02_24_170154_add_softdelete_to_savings.php
+++ database/migrations/2015_02_24_170154_add_softdelete_to_savings.php
@@ -6,11 +6,15 @@ use Illuminate\Database\Migrations\Migration;
 class AddSoftdeleteToSavings extends Migration {
 
     public function up() {
-        
+        Schema::table('savings', function($table) {
+            $table->softDeletes();
+        });
     }
 
     public function down() {
-        
+        Schema::table('savings', function($table) {
+            $table->dropSoftDeletes();
+        });
     }
 
 }
