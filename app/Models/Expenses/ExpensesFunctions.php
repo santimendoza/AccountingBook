ac4ac1619278e873ab01e4fe6b21a0f5ee352c5c
Santiago Mendoza Ramirez <contacto@santiagomendoza.org>
2015-03-03 16:36:18 -0500

    Solo gastos del mes

--- app/Models/Expenses/ExpensesFunctions.php
+++ app/Models/Expenses/ExpensesFunctions.php
@@ -13,12 +13,17 @@ class ExpensesFunctions {
     }
 
     public static function calculateExpensesCategory($category) {
-        $expenses = Expenses::where('expensesCategory_id', '=', $category->id)->get();
+        $m = date('n');
+        $monthstartday = date('Y-m-d', mktime(1, 1, 1, $m, 1, date('Y')));
+        $monthendday = date('Y-m-d', mktime(1, 1, 1, $m + 1, 0, date('Y')));
+        $monthstartdaystring = str_replace('-', '', $monthstartday);
+        $monthenddaystring = str_replace('-', '', $monthendday);
+        $expenses = Expenses::where('expensesCategory_id', '=', $category->id)->where('date', '>=', $monthstartdaystring)->get();
         $totalexpenses = 0;
         foreach ($expenses as $expense) {
             $totalexpenses += $expense->amount;
         }
-        
+
         return $totalexpenses;
     }
 
