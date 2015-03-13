5daa1439f47df5b7da605e525f794847c3da6b34
Santiago Mendoza Ramirez <contacto@santiagomendoza.org>
2015-03-13 10:07:36 -0500

    Arreglar vista de presupuestos.

--- resources/views/budget/index.blade.php
+++ resources/views/budget/index.blade.php
@@ -11,57 +11,46 @@
         </div>
         <h1>Presupuesto</h1>
     </div>
-    <div class="col-sm-12">
-        <h2>Gastos</h2>
-        @foreach($categories[0] as $categorysup)
-        <div class="col-sm-3">
-            <div class="panel panel-primary">
-                <div class="panel-heading">
-                    <h3 class="panel-title">{{$categorysup->slug}}</h3>
-                </div>
-                <div class="panel-body">
-                    <p><b>Presupuestado</b>: {{$categorysup->budget}}</p>
-                    <p>
-                        <b>Gastado</b>: <span class="@if($categorysup->amount > $categorysup->budget)text-color-red @endif">{{$categorysup->amount}}</span>
-                    </p>
-                    <p>
-                        <b>Restante</b>: <span class="@if($categorysup->amount > $categorysup->budget)text-color-red @else text-color-green @endif">{{$categorysup->budget - $categorysup->amount }}</span>
-                    </p>
+    <div class="row">
+        <div class="col-sm-12">
+            <h2>Gastos</h2>
+            @foreach($categories[0] as $categorysup)
+            <div class="col-sm-3">
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">{{$categorysup->slug}}</h3>
+                    </div>
+                    <div class="panel-body">
+                        <p><b>Presupuestado</b>: {{$categorysup->budget}}</p>
+                        <p>
+                            <b>Gastado</b>: <span class="@if($categorysup->amount > $categorysup->budget)text-color-red @endif">{{$categorysup->amount}}</span>
+                        </p>
+                        <p>
+                            <b>Restante</b>: <span class="@if($categorysup->amount > $categorysup->budget)text-color-red @else text-color-green @endif">{{$categorysup->budget - $categorysup->amount }}</span>
+                        </p>
+                    </div>
                 </div>
             </div>
-            @foreach($categories[1] as $categoryinf)
-            @foreach($categoryinf as $category)
-            @if($categorysup->superior_cat == $categorysup->id)
-            <div id="piechart{{$categoryinf->id}}">
-                <h1>{{$categoryinf->slug}}</h1>
-                <p>Presupuestado: {{$categoryinf->budget}}</p>
-                <p>Gastado: <span class="text-color-red">{{$categoryinf->amount}}</span></p>
-                <p class="text-color-green">Restante: {{$categoryinf->budget - $categoryinf->amount }}</p>
-            </div>
-            @endif
             @endforeach
-            @endforeach
-
         </div>
-        @endforeach
-        <h2>Ahorros</h2>
-
-        @foreach($savings as $saving)
-
-        <div class="col-sm-3">
-            <div class="panel panel-primary">
-                <div class="panel-heading">
-                    <h3 class="panel-title">{{$saving->title}}</h3>
-                </div>
-                <div class="panel-body">
-                    <p><b>Presupuestado</b>: {{$saving->budget}}</p>
-                    <p><b>Ahorrado</b>: {{$saving->expenses}}</p>
+    </div>
+    <div class="row">
+        <div class="col-sm-12">
+            <h2>Ahorros</h2>
+            @foreach($savings as $saving)
+            <div class="col-sm-3">
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">{{$saving->title}}</h3>
+                    </div>
+                    <div class="panel-body">
+                        <p><b>Presupuestado</b>: {{$saving->budget}}</p>
+                        <p><b>Ahorrado</b>: {{$saving->expenses}}</p>
+                    </div>
                 </div>
             </div>
+            @endforeach
         </div>
-
-        @endforeach
-
     </div>
 </div>
 
