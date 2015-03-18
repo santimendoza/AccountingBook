e375b2e42487907198fc5d2acdfe6e662e656390
Santiago Mendoza Ramirez <contacto@santiagomendoza.org>
2015-03-18 15:59:32 -0500

    Agregar rutas para api con auth.basic
    Agregadas rutas para la api, de modo que quede el prefijo /api/ para
    todas las peticiones de la api. Para estas rutas, el middleware es
    auth.basic, y se puede enviar username +  password para las peticiones.
    
    Falta actualizar los controladores.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -33,6 +33,39 @@ Route::group(['middleware' => 'auth'], function() {
     Route::get('/dashboard', 'DashboardController@dashboard');
 });
 
+Route::group(['prefix' => 'api' ,'middleware' => 'auth.basic'], function() {
+    //Categories routes
+    Route::resource('/categories/earnings', 'EarningsCategoriesController');
+    Route::resource('/categories/expenses', 'ExpensesCategoriesController');
+    Route::post('/categories/expenses/{id}/reports', 'ReportsController@expensesCategoryReport');
+
+    //Earnings
+    Route::resource('/earnings', 'EarningsController');
+    Route::post('/earnings/reports', 'ReportsController@earningsreport');
+
+    //Expenses
+    Route::resource('/expenses', 'ExpensesController');
+    Route::post('/expenses/reports', 'ReportsController@expensesreport');
+
+    //Savings
+    Route::resource('/savings', 'SavingsController');
+    Route::get('/savings/{id}/add', 'SavingsController@addFounds');
+    Route::post('/savings/{id}/updateAmount', 'SavingsController@updateAmount');
+    Route::get('/savings/{id}/use', 'SavingsController@useFounds');
+    Route::post('/savings/{id}/usedFounds', 'SavingsController@usedFounds');
+
+    //Budget
+    Route::resource('/budget', 'BudgetController');
+    Route::post('/budget/savings-budget', 'BudgetController@storeSavingsBudget');
+
+    //User Routes
+    Route::resource('/user', 'UserController');
+    Route::get('/user/{{username}}', 'UserController@show');
+
+    //Dashboard
+    Route::get('/dashboard', 'DashboardController@dashboard');
+});
+
 //General Routes
 Route::get('/', function() {
     return redirect('/auth/login');
